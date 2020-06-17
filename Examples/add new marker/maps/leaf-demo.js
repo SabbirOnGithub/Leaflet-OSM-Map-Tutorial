d2e77ca7d98d1ca90777cc434f47dca7cd285b26
Sabbir <sabbir@pbazaar.com>
2020-06-18 00:29:45 +0600

    initial Commit

---
+++ Examples/add new marker/maps/leaf-demo.js
@@ -0,0 +1,48 @@
+// See post: http://asmaloney.com/2014/01/code/creating-an-interactive-map-with-leaflet-and-openstreetmap/
+
+var map = L.map( 'map', {
+  center: [20.0, 5.0],
+  minZoom: 2,
+  zoom: 2
+})
+
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: ['a', 'b', 'c']
+}).addTo( map )
+
+var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' )
+
+var myIcon = L.icon({
+  iconUrl: myURL + 'images/pin24.png',
+  iconRetinaUrl: myURL + 'images/pin48.png',
+  iconSize: [29, 24],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14]
+})
+
+var popup = L.popup();
+
+for ( var i=0; i < markers.length; ++i )
+{
+ L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
+  .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' )
+  .addTo( map );
+}
+
+var marker = {};
+
+function onMapClick(e) {
+
+var lat = e.latlng.lat;
+var lng = e.latlng.lng;
+
+console.log("You clicked the map at LAT: "+ lat+" and LONG: "+lng );
+
+  if(marker){
+    map.removeLayer(marker)
+  }
+  marker = L.marker([lat, lng], {icon: myIcon} ).addTo(map)
+}
+
+map.on('click', onMapClick);
